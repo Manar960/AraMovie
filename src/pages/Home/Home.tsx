baf26117be44e89e9448a843820a0d91a3b1b5ab
Manar Jber <mohamadmanar960@gmail.com>
2024-09-12 17:12:18 +0300

    feat: create top rated section and footer

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,10 @@ import Hero from "./components/Hero";
 import "./components/Home.css";
 import Shows from "./components/Shows";
 import UpcomingMovies from "./components/UpcomingMovies";
-import { movieData ,asiaData,animeData} from "../../assets/data";
+import { movieData, asiaData, animeData } from "../../assets/data";
 import News from "./components/News";
-
+import Footer from "./components/Footer";
+import TopRated from "./components/TopRated";
 
 const Home = () => {
   return (
@@ -16,7 +17,9 @@ const Home = () => {
         <Shows title="Recently Added Movies" images={movieData} />
         <Shows title="Asian Shows" noBackground images={asiaData} />
         <Shows title="Anime" images={animeData} />
-        <News/>
+        <TopRated />
+        <News />
+        <Footer />
       </article>
     </main>
   );
